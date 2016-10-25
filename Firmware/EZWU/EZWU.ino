f0a378b1e0c0f9bba6ba35295de099bebafdd83f
Sergio Vargas <savd_08@outlook.com>
2016-10-25 03:19:29 -0600

    Agrega rutina de alarma, mensajes de debugging, lectura de sensor ultrasónico, servos, buzzer

--- Firmware/EZWU/EZWU.ino
+++ Firmware/EZWU/EZWU.ino
@@ -1,16 +1,20 @@
+#include <Servo.h>
 #include <ESP8266WiFi.h>
 
-#define MODE_SW 2   //Interruptor de modo: 1 config, 0 operación
+#define MODE_SW 2   //Interruptor de modo: 0 config, 1 operación
 #define ALM_BT 0    //Botón de apagado de alarma (podría ser RST)
 #define SRV_L 5     //Servo izquierdo
 #define SRV_R 4     //Servo derecho
-#define TRIG 7      //Señal trigger para sensor ultrasónico
-#define ECHO 8      //Señal echo de sensor ultrasónico
+#define TRIG 1      //Señal trigger para sensor ultrasónico
+#define ECHO 3      //Señal echo de sensor ultrasónico
 #define BUZZER 15   //Señal de control para el buzzer
 
 #define DEBUG
 
+Servo servo_l;
+Servo servo_r;
 WiFiClient client;
+
 
 char ssid[33] = "OP";   //SSID de red para el dispositivo
 char pass[33] = "pass"; //Contraseña de la red
@@ -19,61 +23,153 @@ uint8_t sleep_time = 60;  //Tiempo en segundos para el estado de bajo consumo
 
 uint16_t smoke_thresh = 900;  //Valor umbral para la concentración de humo
 
+uint16_t dist_thresh = 30;  //Valor umbral para la concentración de humo
+
 uint16_t alarm_time = 450; //Hora de la alarma en minutos transcurridos desde 00:00 (60*h + m)
 
-void setup(){
-  init();   //Inicialización de hardware
+unsigned long t0;
+unsigned int rand_delay;
 
+void setup(){
+  //ESP.wdtEnable(2000);
+  //yield();
+  dev_setup();   //Inicialización de hardware
+  
+  #ifdef DEBUG
+      Serial1.println("Check 3");
+   #endif
   //Entra a modo config si GPIO2 está en alto
-  if(digitalRead(MODE_SW)){
+  if(false){
     //Modo config
   } else {
+    yield();
+    #ifdef DEBUG
+      Serial1.println("Modo Operación");
+    #endif
     
     //Lectura del ADC (sensor de humo)
     uint16_t smoke_adc = analogRead(A0);
     
+    #ifdef DEBUG
+      Serial1.print("ADC: ");
+      Serial1.println(smoke_adc);
+    #endif
+    
     //Si se rompe el umbral se sale de la función setup para continuar con loop 
     if(smoke_adc > smoke_thresh){
+      #ifdef DEBUG
+        Serial1.println("Alarma por humo");
+      #endif
+      timer_reset();
       return;
-    }
+    } else {
 
-    //Se conecta a la red WiFi
-    connect_wifi();
-    
-    //Se realiza request HEAD a google.com para obtener la hora
-    String time_resp = "";
-    if(get_time(&time_resp)){
+      //Se conecta a la red WiFi
+      connect_wifi();
       
-      //Se parsea la hora de la respuesta de la solicitud a formato 60*h + m
-      int gmt_time = parse_time(time_resp);
-
-      //Si es la hora programada se sale de la función setup para continuar con loop 
-      if(gmt_time == alarm_time){
-        return
+      //Se realiza request HEAD a google.com para obtener la hora
+      String time_resp = "";
+      if(get_time(&time_resp)){
+        
+        #ifdef DEBUG
+          Serial1.print("HEAD: ");
+          Serial1.println(time_resp);
+        #endif
+        
+        //Se parsea la hora de la respuesta de la solicitud a formato 60*h + m
+        int gmt_time = parse_time(time_resp);
+        
+        #ifdef DEBUG
+          Serial1.print("Hora formateada: ");
+          Serial1.println(gmt_time);
+        #endif
+  
+        //Si es la hora programada se sale de la función setup para continuar con loop 
+        if(gmt_time == alarm_time){
+          #ifdef DEBUG
+            Serial1.println("Alarma por hora");
+          #endif
+          timer_reset();
+          //ESP.wdtEnable(1000);
+          return;
+        }
       }
     }
 
+    #ifdef DEBUG
+      Serial1.println("Deep sleep");
+      Serial1.println();
+    #endif
     //Se entra en estado de bajo consumo durante el tiempo especificado
-    ESP.deepSleep(sleep_time * 1000000);
+    //ESP.deepSleep(sleep_time * 1000000);
+    ESP.restart();
   }
 }
 
-void loop(){
-  
+void timer_reset(){
+  t0 = millis();
+  randomSeed(analogRead(A0));
+  rand_delay = random(1000,4000);
 }
 
-void init(){
-  //Se colocan varios pines del sistema en el estado que se requiere
-  pinMode(MODE_SW, INPUT);
-  pinMode(ALM_BT, INPUT);
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
-  pinMode(BUZZER, OUTPUT);
+void loop(){
+  unsigned long t = (millis() - t0);
+  
+  bool los_blocked = false;
+  bool buzzer_on = false;
+  bool rotate = false;
+  
+  los_blocked = (read_ultrasonic() < dist_thresh) ? false : true;
+  buzzer_on = (t%450 < 225) ? true : false;
+  digitalWrite(BUZZER, buzzer_on);
 
+  if(t < rand_delay && !rotate){
+    if(!los_blocked){
+      servo_l.writeMicroseconds(1700);
+      servo_r.writeMicroseconds(1300);
+    } else {
+      servo_l.writeMicroseconds(1400);
+      servo_r.writeMicroseconds(1400);  
+    }
+  } else {
+    if(!rotate){
+      timer_reset();
+      rotate = true;
+      return;
+    }
+    if(t < rand_delay){
+      servo_l.writeMicroseconds(1700);
+      servo_r.writeMicroseconds(1700);
+    } else {
+      rotate = false;  
+    }
+  }
+}
+
+void dev_setup(){
   //Línea de comunicación de depuración
   #ifdef DEBUG
-    Serial1.begin(9600);
+    Serial1.begin(115200);
+    Serial1.println("Init");
   #endif
+  
+  //Se colocan varios pines del sistema en el estado que se requiere
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  #ifdef DEBUG
+    Serial1.println("Check 1");
+  #endif
+  
+  //pinMode(MODE_SW, INPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(ALM_BT, INPUT);
+
+  #ifdef DEBUG
+    Serial1.println("Check 2");
+  #endif
+  servo_l.attach(SRV_L);
+  servo_r.attach(SRV_R);
 }
 
 void connect_wifi()
@@ -90,7 +186,14 @@ void connect_wifi()
     i++;
     delay(100);
   }
+  
+  #ifdef DEBUG
+    if(WiFi.status() == WL_CONNECTED){
+      Serial1.println("Wifi conectado");
+    }
+  #endif
 }
+
 bool get_time(String *response) {
 
   //Se intenta conectar con el servidor y puerto dados
@@ -111,6 +214,7 @@ bool get_time(String *response) {
     char c = client.read();
     response->concat(c);
     i++;
+    delay(10);
   }
   
   return true;
@@ -121,5 +225,15 @@ int parse_time(String time_str){
   uint8_t hour = time_str.substring(time_index, time_index + 2).toInt();
   uint8_t minute = time_str.substring(time_index + 2, time_index + 4).toInt();
   return 60*hour + minute;
+}
+
+uint16_t read_ultrasonic(){
+  unsigned long tof = pulseIn(ECHO, HIGH);
+  uint16_t dist_cm = tof/58;
+  #ifdef DEBUG
+        Serial1.print("Dist: ");
+        Serial1.println(dist_cm);
+  #endif
+  return dist_cm;
 }
 
